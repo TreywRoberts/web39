a79495a6cbfe2c3a616b35ac7479eb5478248613
Trey <treywroberts@gmail.com>
2021-03-11 12:35:54 -0500

    server started

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+console.log('hello')
+const express = require('express')
+
+
+const server = express()
+
+server.use(express.json())
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'development') {
+    const cors = require('cors')
+    server.use(cors())
+}
