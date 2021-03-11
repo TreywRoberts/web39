1be25f985707bfacd4861f4436e394ebf8298e2a
Trey <treywroberts@gmail.com>
2021-03-11 13:31:04 -0500

    wip

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require('path')
 
 const express = require('express')
 
@@ -6,6 +7,7 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
 console.log(process.env.NODE_ENV)
 
 if (process.env.NODE_ENV === 'development') {
@@ -13,9 +15,14 @@ if (process.env.NODE_ENV === 'development') {
     server.use(cors())
 }
 
-server.use('*', (req, res)=>{
-    res.send('<h1>success</h1>')
+server.get('/api/hello', (req,res)=>{
+    res.json({message: 'Hello'})
 })
+
+server.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
 const PORT = process.env.PORT || 4000
 server.listen(PORT, ()=>{
     console.log(`listing on ${PORT}`)
